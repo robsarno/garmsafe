801bbffb9dc05d11986585c102c0c8ec87e8d7a0
robsarno <roberto.arnoldi.1999@gmail.com>
2018-01-13 13:21:14 +0100

    Aggiornati link footer.

--- resources/views/templates/footer.blade.php
+++ resources/views/templates/footer.blade.php
@@ -3,16 +3,16 @@
 		<div class="row">
 			<div class="col-md-4 text-center">
 				<h2>Link Utili</h2>
-				<p><a href="http://garmsafe.altervista.org">Home</a></p>
-				<p><a href="http://garmsafe.altervista.org/pagine/pagina-contatti.php">Contatti</a></p>
-				<p><a href="http://garmsafe.altervista.org/pagine/pagina-chisiamo.php?partners">Partners</a></p>
-				<p><a href="http://garmsafe.altervista.org/pagine/pagina-copyright.php">Copyright</a></p>
+				<p><a href="/">Home</a></p>
+				<p><a href="/contatti">Contatti</a></p>
+				<p><a href="/chi-siamo">Chi siamo</a></p>
+				<p><a href="/copyright">Cookies e privacy</a></p>
 			</div>
 			<div class="col-md-4 text-center">
 				<h2>Dati società</h2>
 				<p>GARMSAFE SRL</p>
-				<p>Via Alberico da rosciate 11/13</p>
-				<p>24124, BERGAMO [BG]</p>
+				<p>Via Lombardia, 34</p>
+				<p>20056, Trezzo sull'Adda [MI]</p>
 				<p>Telefono:<em>035215568</em></p>
 				<p><a href="mailto:garmsafe@gmail.com">Mail: garmsafe@gmail.com</a></p>
 			</div>
